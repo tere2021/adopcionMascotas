ff257c965ecb17703f829e7136a17cd15a7129e6
Teresita Gallardo <85501870+tere2021@users.noreply.github.com>
2022-10-15 14:46:00 -0300

    json c carrusel bootstrap

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-/* usar json
-pasar los objetos a formato array
-luego generar el recorrido al dom 
-usar un map*/
-
-
-
-const resultado = 
-Object.keys(data) // Primero obtienes las llaves del objeto, es decir 1,2,3 en un array
-.map( key => ({ id: key, value: data[key]  }) ); // Luego lo ciclas y haces un nuevo array, referenciando al original
-
-console.log(resultado);
+function jsonAcarrusel(){
+    let http = new XMLHttpRequest();
+    http.open('get', 'https://tere2021.github.io/adopcionMascotas/mascotas.json', true);
+    http.send();
+ 
+    http.onload = function(){
+       if(this.readyState == 4 && this.status == 200){
+          let mascotas = JSON.parse(this.responseText);
+          let output=''; let act='';
+          
+          for(let x of mascotas){
+             console.log(x);  
+             if (act==='') {
+                act="active";
+                output += ` 
+                <div class="carousel-item active" >  
+                <div class="card pb-5">
+                   <img src="${x.img}" class="d-block w-100" alt="${x.nombre}">
+                   <div class="card-body" >
+                      <h3 class="card-title text-uppercase font-weight-bold" >${x.nombre}</h3>
+                      <span class="badge bg-warning text-dark">Edad: ${x.edad}</span>
+                      <span class="badge bg-danger">Sexo: ${x.sexo}</span> 
+                      <p class="card-text  py-2">Características: <b> ${x.cualidades}, ${x.caracter}</b></p>
+                      <button type="button" class="btn btn-primary btn-sm ">Adoptame</button>
+                   </div>
+                </div>
+             </div> 
+                 `;
+             } else{
+              
+                output += ` 
+                <div class="carousel-item" >  
+                   <div class="card pb-5">
+                      <img src="${x.img}" class="d-block w-100" alt="${x.nombre}">
+                      <div class="card-body" >
+                      <h3 class="card-title text-uppercase font-weight-bold" >${x.nombre}</h3>
+                      <span class="badge bg-warning text-dark">Edad: ${x.edad}</span>
+                      <span class="badge bg-danger">Sexo: ${x.sexo}</span>
+                      <p class="card-text  py-2">Características: <b>${x.cualidades}, ${x.caracter}</b></p>
+                         <button type="button" class="btn btn-primary btn-sm ">Adoptame</button>
+                      </div>
+                   </div>
+                </div> 
+                 `;
+             } 
+  
+          }
+           document.getElementById("cardItem").innerHTML = output;
+       }
+    }
+ }  
+ 
+ jsonAcarrusel();
+ 
